71f9e694b72a19948349cadda74c1597335c2f97
VictorVonKrautz <68201348+VictorVonKrautz@users.noreply.github.com>
2022-09-06 08:04:16 -0500

    Add files via upload

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ function PageTransitions(){
         }
 
     })
+
+    //toggle theme
+    const themeBtn= document.querySelector(".theme-btn");
+    themeBtn.addEventListener("click", () =>{
+        let element = document.body;
+        element.classList.toggle("light-mode")
+    })
 }
 
 PageTransitions();
